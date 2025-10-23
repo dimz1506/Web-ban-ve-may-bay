c6ce722d22125224d38535c3cfdcea6366b3ec59
haikente <lehai30042004@gmail.com>
2025-10-23 16:19:35 +0700

    update

--- airline/config.php
+++ airline/config.php
@@ -2,6 +2,12 @@
 // config.php
 declare(strict_types=1);
 session_start();
+if (!function_exists('current_user_role')) {
+    function current_user_role() {
+        return $_SESSION['user']['role'] ?? null;
+    }
+}
+
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 
 define('DB_HOST', '127.0.0.1');
@@ -9,7 +15,7 @@ define('DB_NAME', 'dat_ve_may_bay');
 define('DB_USER', 'root');
 define('DB_PASS', ''); // đổi cho phù hợp
 
-define('APP_BASE', '/web-ban-ve-may-bay/airline/public'); // đổi cho phù hợp
+define('APP_BASE', '/Web-ban-ve-may-bay/airline/public'); // đổi cho phù hợp
 
 define('APP_NAME', 'Airline Manager');
 // session_start();
